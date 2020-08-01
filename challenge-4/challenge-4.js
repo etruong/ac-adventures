5a5bdbbdd44bbedcccf8c2a1ea0ab1a4dee0e2ed
Elisa <elisat15@uw.edu>
2020-08-01 14:43:00 -0700

    clear work

--- challenge-4/challenge-4.js
+++ challenge-4/challenge-4.js
@@ -40,60 +40,28 @@ const store = {
 const storeItems = Object.keys(store); // array of all items sold in store
 // storeItems = ["apple", "peach", "pear", "cherry", "orange", "banana", "durian"]
 
-const dropdown = document.querySelector("select");
-for (let i = 0; i < storeItems.length; i++) {
-    let option = document.createElement("option");
-    option.textContent = storeItems[i];
-    option.value = storeItems[i];
-    dropdown.appendChild(option);
-}
 
 // Step 2: Create the createFruit function
 function createFruit(type, info) {
-    const fruit = document.createElement("img");
-    fruit.src = info.src;
-    fruit.alt = type;
-    fruit.classList.add("item");
-    fruit.classList.add(type);
-    fruit.addEventListener("click", function () {
-        fruit.classList.toggle("selected");
-    });
-    return fruit;
+   
 }
 
 // Step 3: Populate the store by creating the populateStore function and calling it
 function populateStore() {
-    const storeContainer = document.getElementById("store");
-    for (let i = 0; i < storeItems.length; i++) {
-        let fruit = storeItems[i];
-        let info = store[fruit];
-        for (let num = 0; num < info.count; num++) {
-            storeContainer.appendChild(createFruit(fruit, info));
-        }
-    }
+
 }
-populateStore();
 
 // Step 4: Develop the sell function 
 function sell() {
-    const selectedItems = document.querySelectorAll(".selected");
-    let total = 0;
-    for (let i = 0; i < selectedItems.length; i++) {
-        let fruit = selectedItems[i];
-        total += store[fruit.alt].price;
-        fruit.remove();
-    }
-    changeProfit(total);
+
 }
 
 // Step 5
-const sellBtn = document.getElementById("sell-btn");
-sellBtn.addEventListener("click", sell);
+
 
 // Step 6
 function changeProfit(num) {
-    let profit = document.querySelector("#profit");
-    profit.textContent = parseInt(profit.textContent) + num;
+
 }
 
 // Optional Challenge Code Goes Here:
